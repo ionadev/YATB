d35f9b3894a73b7935afa26c224936f2a78976be
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-03-15 20:11:01 +0530

    yo

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on("ready", () => {
-    console.log("[ BOT ] - Conected To Discord");
-});
-client.login(process.env.token);
-//# sourceMappingURL=index.js.map
