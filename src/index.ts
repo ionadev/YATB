b55fafb54c902f3b448244870a9e0bfe659bf7fd
Piyush <bhangalepiyush@gmail.com>
2019-03-17 16:39:41 +0530

    Yo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import * as Discord from 'discord.js';
 
 const client = new Discord.Client();
+const PREFIX = "!";
 
 client.on("ready", () => {
     console.log("[ BOT ] - Conected To Discord");
+});
+
+client.on('message', (msg: Discord.Message) => {
+    if(msg.author.bot) return;
+    if(msg.content.indexOf(PREFIX) !== 0) return;
+    const args = msg.content.slice(PREFIX.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 });
 
 client.login(process.env.token);
